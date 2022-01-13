774a4cb60cb70565f309b584dab69f56cbff8322
msabinin <73134009+msabinin@users.noreply.github.com>
2022-01-13 21:33:36 +0300

    Update gunicorn_django.py

--- web/ask/gunicorn_django.py
+++ web/ask/gunicorn_django.py
@@ -1,4 +1,12 @@
 #wsgi_django.py
-
-bind =  '0.0.0.0:8000'
-wsgi_app = 'ask.wsgi:application'
+CONFIG = {
+		'mode':'wsgi',
+		'working_dir':'/home/box/web/ask',
+		'python':'/usr/bin/python',
+		'args':(
+			'--bind=0.0.0.0:8000',
+			'--workers=1',
+			'--timeout=60',
+			'ask.wsgi:application'
+		)
+	}
