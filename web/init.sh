33b49f759bf340a42747a33921ff318a96a21a42
msabinin <73134009+msabinin@users.noreply.github.com>
2022-01-13 16:49:44 +0300

    Update init.sh

--- web/init.sh
+++ web/init.sh
@@ -4,6 +4,7 @@ nginx='sudo ln -s $base_dir/etc/nginx.conf /etc/nginx/sites-enabled/test.conf'
 eval $nginx
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo apt-get install --only-upgrade gunicorn
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
