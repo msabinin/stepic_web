45e5f19852b1d714fa41fbd1b3a64c6ae27d2fc8
msabinin <73134009+msabinin@users.noreply.github.com>
2022-01-20 10:43:04 +0300

    Update init.sh

--- web/init.sh
+++ web/init.sh
@@ -10,4 +10,5 @@ cd ask
 gunicorn ask.wsgi:application --bind 0.0.0.0:8000 &
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
-#sudo /etc/init.d/mysql start
+sudo /etc/init.d/mysql start
+sudo mysql -u root -e "create database stepic_web; create user box@localhost; grant all privileges on stepic_web.* to box@localhost with grant option;"
