5890e0ad584cb110cb0237120bd6e6cf4b6a79f5
msabinin <m.sabinin@yandex.ru>
2022-02-04 11:33:02 +0300

    modified:   web/ask/qa/forms.py

--- web/ask/qa/forms.py
+++ web/ask/qa/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.forms import ModelForm
 from qa.models import Question, Answer
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 class AskForm(forms.Form):
     title = forms.CharField(max_length=100)
@@ -67,9 +68,41 @@ class LoginForm(forms.Form):
     def save(self):
         auth = authenticate(**self.cleaned_data)
         return auth
-'''
 
 class SignupForm(ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+'''
+
+class SignupForm(forms.Form):
+    username = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if username.strip() == '':
+            raise forms.ValidationError(
+                u'Username is empty', code='validation_error')
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if email.strip() == '':
+            raise forms.ValidationError(
+                u'Email is empty', code='validation_error')
+        return email
+
+    def clean_password(self):
+        password = self.cleaned_data['password']
+        if password.strip() == '':
+            raise forms.ValidationError(
+                u'Password is empty', code='validation_error')
+        return password
+
+    def save(self):
+        user = User.objects.create_user(**self.cleaned_data)
+        user.save()
+        auth = authenticate(**self.cleaned_data)
+        return auth
